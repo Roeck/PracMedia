734b630197caa5be17758dfaaaf2055741e15428
rons9n <rons9n@gmail.com>
2022-09-21 17:53:02 -0500

    Firebase setup

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA0TjVSf2uEKCSsmBokd51F9wlWnAhTgFo",
+    authDomain: "pracmedia.firebaseapp.com",
+    projectId: "pracmedia",
+    storageBucket: "pracmedia.appspot.com",
+    messagingSenderId: "843954077857",
+    appId: "1:843954077857:web:a2fb1fe02a59d858e630e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
