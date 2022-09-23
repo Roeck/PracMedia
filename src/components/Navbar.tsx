213eaa819ee4bfa06d0c5c1bc2a1f0e36ad241a8
rons9n <rons9n@gmail.com>
2022-09-22 21:51:52 -0500

    Firebase auth done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { signOut } from "firebase/auth";
 
 export const Navbar = () => {
     return (
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+            <Link to="/"> Home </Link>
+            <Link to="/login"> Login </Link>
+            <div>
+                <p> {auth.currentUser?.displayName} </p>
+                <img src={auth.currentUser?.photoURL || ""} width="20" height="20" />
+            </div>
         </div>
-    )
-}
+    );
+};
